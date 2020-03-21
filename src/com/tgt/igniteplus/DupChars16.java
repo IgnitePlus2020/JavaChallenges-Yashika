8f3a5aef8a0857db904ceb22ec0ff338a641321e
YashikaBC <61619152+YashikaBC@users.noreply.github.com>
2020-03-21 13:00:01 +0530

    Update DupChars16.java

--- src/com/tgt/igniteplus/DupChars16.java
+++ src/com/tgt/igniteplus/DupChars16.java
@@ -3,17 +3,23 @@ import java.util.*;
 public class DupChars16 {
     public static void main(String[] args){
         String str;
+        int count;
         System.out.print("enter the string");
         Scanner in=new Scanner(System.in);
         str=in.nextLine();
-        char[] input=str.toCharArray();
+        char[] in=str.toCharArray();
         System.out.println("the duplicate characters are");
-        for(int i=0;i<str.length();i++) {
-            for (int j = i + 1; j < str.length(); j++){
-                if (input[i] == input[j])
-                    System.out.print(input[j]);
-            }
-        }
-    }
-}
-
+        for(int i = 0; i <str.length; i++) {  
+            count = 1;  
+            for(int j = i+1; j <str.length; j++) {  
+                if(in[i] == string[j] && in[i] != ' ') {  
+                    count++;  
+                    in[j] = '0';  
+                }  
+            }  
+ 
+            if(count > 1 && in[i] != '0')  
+                System.out.println(in[i]);  
+        }  
+    }  
+}  
